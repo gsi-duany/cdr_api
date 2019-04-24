59b9e23505e527350dc85fa7c99e0717edaf220a
gsi-duany <duany@generalsoftwareinc.com>
2019-04-24 10:57:15 -0800

    fixed: Fixed models' cdr

--- cdr/models.py
+++ cdr/models.py
@@ -14,26 +14,35 @@ DISPOSITION = (
 
 class Cdr(models.Model):
     calldate = models.DateTimeField()
-    clid = models.CharField(max_length=240)
-    src = models.CharField(max_length=240)
-    dst = models.CharField(max_length=240)
-    dcontext = models.CharField(max_length=240)
-    channel = models.CharField(max_length=240)
-    dstchannel = models.CharField(max_length=240)
-    lastapp = models.CharField(max_length=240)
-    lastdata = models.CharField(max_length=240)
+    clid = models.CharField(max_length=80)
+    src = models.CharField(max_length=80)
+    dst = models.CharField(max_length=80)
+    dcontext = models.CharField(max_length=80)
+    channel = models.CharField(max_length=80)
+    dstchannel = models.CharField(max_length=80)
+    lastapp = models.CharField(max_length=80)
+    lastdata = models.CharField(max_length=80)
     duration = models.IntegerField()
     billsec = models.IntegerField()
-    disposition = models.CharField(max_length=135)
+    disposition = models.CharField(max_length=45)
     amaflags = models.IntegerField()
-    accountcode = models.CharField(max_length=60)
-    uniqueid = models.CharField(max_length=96, primary_key=True)
-    userfield = models.CharField(max_length=765)
+    accountcode = models.CharField(max_length=20)
+    uniqueid = models.CharField(max_length=32)
+    userfield = models.CharField(max_length=255)
+    did = models.CharField(max_length=50)
+    recordingfile = models.CharField(max_length=255)
+    cnum = models.CharField(max_length=80)
+    cnam = models.CharField(max_length=80)
+    outbound_cnum = models.CharField(max_length=80)
+    outbound_cnam = models.CharField(max_length=80)
+    dst_cnam = models.CharField(max_length=80)
+    linkedid = models.CharField(max_length=32)
+    peeraccount = models.CharField(max_length=80)
+    sequence = models.IntegerField()
 
     class Meta:
-        db_table = u'cdr'
-        verbose_name = 'registro de llamada'
-        verbose_name_plural = 'registros de llamadas'
+        managed = False
+        db_table = 'cdr'
 
     def __unicode__(self):
         return 'callid: %s "%s" (%i)' % (self.uniqueid, self.disposition, self.duration)
