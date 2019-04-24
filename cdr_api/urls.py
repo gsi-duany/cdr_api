9451d8daf28ce95d2a2c6c6ef73a335c4f370139
gsi-duany <duany@generalsoftwareinc.com>
2019-04-24 11:56:27 -0800

    fixed: Fixed cdr's url

--- cdr_api/urls.py
+++ cdr_api/urls.py
@@ -24,7 +24,7 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='Pastebin API')
 
 router = routers.DefaultRouter()
-router.register(r'cdrs', CdrViewSet)
+router.register(r'api/cdrs', CdrViewSet)
 
 
 urlpatterns = [
